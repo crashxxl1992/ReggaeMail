20d35d2d897a3a430e769acdd1450e24845ac91c
pja35 <pablo.arrighi@gmail.com>
2014-10-03 12:02:48 +0200

    Ce premier commit a trois mains: - un pour envoyer un mail - un pour lire son inbox - un qui affiche hello

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello.");
+
+	}
+
+}
